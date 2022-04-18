d88c15020b61b9dd8da1adc12079d76196de1c70
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-04-18 22:56:55 +0000

    Add heading 1 and 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <main className="App">
-      
+      <h1>Spacestagram</h1>
+      <h2>Brought to you by NASA's Astronomy Picture of the Day (APOD) API</h2>
     </main>
   );
 }
