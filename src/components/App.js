49f9633753630efd12887c5da0194c36446952e8
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-04-19 00:24:12 +0000

    Add Post component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
+import Post from './Post';
+
 function App() {
   return (
     <main className="App">
       <h1>Spacestagram</h1>
       <h2>Brought to you by NASA's Astronomy Picture of the Day (APOD) API</h2>
+      <Post />
     </main>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+const Post = () => {
+  return (
+    <article>
+      <img src="img.jpg" alt="Description" /> 
+      <h3>Post Title - Date</h3>
+      <button>Like</button>
+    </article>
+  );
+}
+
+export default Post;
