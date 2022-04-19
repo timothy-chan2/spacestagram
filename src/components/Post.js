7ed545cfa21e8130143c208698dd38ad0bfa8a15
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-04-19 02:39:58 +0000

    Add a hardcoded example using data from a local object

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,16 @@
 const Post = () => {
+  const data = {
+    date: "2022-04-18",
+    explanation: "The mission was to document night-flying birds -- but it ended up also documenting a beautiful sky.",
+    hdurl: "https://apod.nasa.gov/apod/image/2204/MwMertola_Claro_2000.jpg",
+    title: 	"Stars and Planets over Portugal"
+  };
+  
   return (
     <article>
-      <img src="img.jpg" alt="Description" /> 
-      <h3>Post Title - Date</h3>
+      <img src={data.hdurl} alt={data.title} /> 
+      <h3>{data.title} ** {data.date}</h3>
+      <p>{data.explanation}</p>
       <button>Like</button>
     </article>
   );
