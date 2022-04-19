00400bd4ef833b73dbe76a4cfabd5225ff48c375
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-04-19 19:15:22 -0400

    Convert the date to a more human readable format with the name of the month

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,6 +3,11 @@ import { useState } from 'react';
 const Post = (props) => {
   const [buttonText, setButtonText] = useState('Like');
 
+  const defaultDate = new Date(props.id);
+  const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+  'August', 'September', 'October', 'November', 'December'][defaultDate.getMonth()];
+  const longDate = `${month} ${defaultDate.getDate() + 1}, ${defaultDate.getFullYear()}`;
+
   const clicked = () => {
     buttonText === 'Like' ? setButtonText('Unlike') : setButtonText('Like');
   }
@@ -10,7 +15,7 @@ const Post = (props) => {
   return (
     <article>
       <img src={props.imgUrl} aria-labelledby={props.id} /> 
-      <h3>{props.title}, {props.id}</h3>
+      <h3>{props.title} - {longDate}</h3>
       <p id={props.id}>{props.description}</p>
       <button
         data-message='Like or unlike the picture'
