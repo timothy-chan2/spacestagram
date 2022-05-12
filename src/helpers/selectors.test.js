30ade13f0caf36efea21e4cfa359bbe58990cf74
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-05-12 20:22:40 +0000

    Add test to check the correct long date is returned by getLongDate function

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -1,6 +1,11 @@
-import { getLongDate } from './selectors';
+import { getLongDate, getCurrentTime } from './selectors';
 
 test('getLongDate returns a string', () => {
   const result = getLongDate(new Date('2022-05-12'));
   expect(typeof result).toBe('string');
+});
+
+test('getLongDate returns the correct long date', () => {
+  const result = getLongDate(new Date(`2022-05-12 ${getCurrentTime()}`));
+  expect(result).toBe('May 12, 2022');
 });
