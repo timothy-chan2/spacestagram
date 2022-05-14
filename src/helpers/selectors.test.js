bb3d509766722b706009afd2dbba1fec5b6272a1
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-05-13 22:54:17 -0400

    Add tests to check that when the date or month is less than 10 that their numerical equivalent are padded in front with a 0

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -10,7 +10,19 @@ test('getLongDate returns the correct long date', () => {
   expect(result).toBe('May 12, 2022');
 });
 
+test('getShortDate returns the date with 2 digits', () => {
+  const result = getShortDate(new Date('Mon May 2, 2022 13:00:00'));
+  const day = result.slice(8, 10);
+  expect(day).toBe('02');
+});
+
+test('getShortDate returns the month with 2 digits', () => {
+  const result = getShortDate(new Date('Mon May 2, 2022 13:00:00'));
+  const month = result.slice(5, 7);
+  expect(month).toBe('05');
+});
+
 test('getShortDate returns the correct short date', () => {
-  const result = getShortDate(new Date('Fri May 13, 2022 22:36:00'));
+  const result = getShortDate(new Date('Fri May 13, 2022 22:30:00'));
   expect(result).toBe('2022-05-13');
 });
