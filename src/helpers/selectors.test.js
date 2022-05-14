c5364c1ee0973cb0d8e9bfdc66ba0ee98723aba4
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-05-13 22:42:51 -0400

    Add test to check that getShortDate function returns the correct short date

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -1,4 +1,4 @@
-import { getLongDate, getCurrentTime } from './selectors';
+import { getLongDate, getCurrentTime, getShortDate } from './selectors';
 
 test('getLongDate returns a string', () => {
   const result = getLongDate(new Date('2022-05-12'));
@@ -8,4 +8,9 @@ test('getLongDate returns a string', () => {
 test('getLongDate returns the correct long date', () => {
   const result = getLongDate(new Date(`2022-05-12 ${getCurrentTime()}`));
   expect(result).toBe('May 12, 2022');
+});
+
+test('getShortDate returns the correct short date', () => {
+  const result = getShortDate(new Date('Fri May 13, 2022 22:36:00'));
+  expect(result).toBe('2022-05-13');
 });
