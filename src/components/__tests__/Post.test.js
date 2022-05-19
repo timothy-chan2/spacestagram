70b306608aef6fbe334bbe6ca6b87d16f6550356
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-05-18 21:07:17 -0400

    Add tests that check the like button changes into an unlike button and vice versa

--- src/components/__tests__/Post.test.js
+++ src/components/__tests__/Post.test.js
@@ -11,3 +11,21 @@ it('renders the default like button', () => {
   render(<Post />);
   expect(screen.getByText('Like')).toBeInTheDocument();
 });
+
+it('renders the unlike button after the like button is clicked', () => {
+  render(<Post />);
+
+  const button = screen.getByText('Like');
+  fireEvent.click(button);
+
+  expect(screen.getByText('Unlike')).toBeInTheDocument();
+});
+
+it('renders the like button after the unlike button is clicked', () => {
+  render(<Post />);
+
+  const button = screen.getByText('Unlike');
+  fireEvent.click(button);
+
+  expect(screen.getByText('Like')).toBeInTheDocument();
+});
