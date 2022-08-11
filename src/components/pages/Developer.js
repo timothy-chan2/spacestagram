f9e8bc014f477f3e1a0a3a6ad6018c179d326204
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-08-11 00:56:00 -0400

    Add route to Developer page

---
+++ src/components/pages/Developer.js
@@ -0,0 +1,9 @@
+const Developer = () => {
+  return (
+    <main>
+      <h2>Developer</h2>
+    </main>
+  );
+};
+
+export default Developer;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import './index.css';
+
 import App from './components/App';
+import Developer from './components/pages/Developer';
+
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="developer" element={<Developer />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
