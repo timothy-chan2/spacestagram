694142306890b94f13df36ab55948a3cace195fd
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-08-18 20:49:42 -0400

    Change the Trash Panda MTL gif into a link to the GitHub page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,17 @@
 const Project = (props) => {
   return (
     <article className='post'>
-      <img
-        src={props.imgUrl}
-        aria-labelledby={props.title}
-        className='post-img'
-      />
+      <a
+        href='https://github.com/andres039/Upcycle-Cradle-Montreal'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <img
+          src={props.imgUrl}
+          aria-labelledby={props.title}
+          className='post-img'
+        />
+      </a>
       <h3 className='post-title'>{props.title}</h3>
       <p className='post-desc project-desc'>{props.description}</p>
     </article>
