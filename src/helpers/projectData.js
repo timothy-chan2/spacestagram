8c17ec38965465bdf0ff27f775982ab87e79eb37
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-08-19 21:22:49 -0400

    Move project data into its own file to help make the Developer component cleaner

---
+++ src/helpers/projectData.js
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    title: 'Trash Panda MTL',
+    imgUrl: 'images/trash-panda.gif',
+    linkUrl: 'https://github.com/andres039/Upcycle-Cradle-Montreal',
+    desc: 'A web app that allows Montrealers to extend the life of discarded furniture by displaying their location on an interactive map.'
+  },
+  {
+    title: 'TinyApp',
+    imgUrl: 'images/tinyapp.gif',
+    linkUrl: 'https://github.com/timothy-chan2/tinyapp',
+    desc: 'A full stack web app built with Node and Express that allows users to shorten long URLs. Users can select between dark/light theme.'
+  }
+];
+
+export default projects;
