f889a7816c209e4abbbe212b359cf17255eca60f
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-08-25 19:33:24 -0400

    Add data related to the Jungle project

--- src/helpers/projectData.js
+++ src/helpers/projectData.js
@@ -6,6 +6,12 @@ const projects = [
     desc: 'A web app that allows Montrealers to extend the life of discarded furniture by displaying their location on an interactive map.'
   },
   {
+    title: 'Jungle',
+    imgUrl: 'images/jungle-rails.png',
+    linkUrl: 'https://github.com/timothy-chan2/jungle-rails',
+    desc: 'An inherited mini e-commerce application built with Rails 4.2 that required bug fixes and additional features.'
+  },
+  {
     title: 'TinyApp',
     imgUrl: 'images/tinyapp.gif',
     linkUrl: 'https://github.com/timothy-chan2/tinyapp',
