bbe4d65c27f4cddccebaa7e4cbecc75cbc9e2ef2
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-11-26 18:24:50 -0500

    Add a console.log to debug why the API is returning an object instead of an array

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,8 @@ const App = () => {
   const [date, setDate] = useState(new Date());
   const [posts, setPosts] = useState([]);
   const apiError = useAppData(date, setPosts);
+
+  console.log('The following are the posts:', posts);
   
   const images = posts.map(post => {
     return (
