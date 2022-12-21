3c0f0eb91e4367812929a8a7dc6d8e241e474719
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-12-21 16:55:46 -0500

    Add an alternate loading animation for the developer page

---
+++ src/components/LoadingDots.js
@@ -0,0 +1,11 @@
+// The LoadingDots component shows three dots that grow and shrink
+// at different times while the project cards are loading
+const LoadingDots = () => {
+  return (
+    <article>
+      
+    </article>
+  );
+};
+
+export default LoadingDots;
