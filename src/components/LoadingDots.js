2daaf9014b9847492745c0b577bece958f4356d2
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-12-21 17:03:56 -0500

    Add the 3 span element that will be used to create the loading dots

--- src/components/LoadingDots.js
+++ src/components/LoadingDots.js
@@ -2,8 +2,10 @@
 // at different times while the project cards are loading
 const LoadingDots = () => {
   return (
-    <article>
-      
+    <article className='loader'>
+      <span className='dot1' />
+      <span className='dot2' />
+      <span className='dot3' />
     </article>
   );
 };
