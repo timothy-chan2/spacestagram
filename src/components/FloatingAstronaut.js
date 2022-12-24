6347abb3631cdeb7a890e2befc8190b649e8f296
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-12-24 00:01:15 -0500

    Add elements for the astronaut

--- src/components/FloatingAstronaut.js
+++ src/components/FloatingAstronaut.js
@@ -10,6 +10,45 @@ const FloatingAstronaut = () => {
         <div className='s5' />
         <div className='s6' />
       </div>
+      <div className='astronaut'>
+        <div className='tank' />
+        <div className='person'>	
+          <div className='helmet'>
+            <div className='glass'>
+              <div className='reflection' />
+            </div>
+          </div>
+					<div className='suit'>
+						<div className='panel'>
+							<div className='btn1' />
+							<div className='btn2' />
+							<div className='knob' />
+						</div>
+					</div>
+					<div className='arml'>
+						<div className='handl'>
+							<div className="glovel">
+								<div className='thumbl' />
+								<div className='wrist2' />
+							</div>
+						</div>
+					</div>
+					<div className='armr'>
+						<div className='handr'>
+							<div className='glover'>
+								<div className='thumbr' />
+								<div className='wrist1' />
+							</div>
+						</div>
+					</div>
+					<div className='legl'>
+						<div className='bootl' />
+					</div>
+					<div class='legr'>
+						<div class='bootr' />
+					</div>
+        </div>
+			</div>
     </article>
   );
 };
