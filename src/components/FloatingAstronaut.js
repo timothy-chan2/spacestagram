f5bab25849d872b4fd2cbb255a41091f534d2de6
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-12-23 23:25:48 -0500

    Add elements for the 6 stars

--- src/components/FloatingAstronaut.js
+++ src/components/FloatingAstronaut.js
@@ -2,7 +2,14 @@
 const FloatingAstronaut = () => {
   return (
     <article className='bkgd'>
-      
+      <div className='stars'>
+        <div className='s1' />
+        <div className='s2' />
+        <div className='s3' />
+        <div className='s4' />
+        <div className='s5' />
+        <div className='s6' />
+      </div>
     </article>
   );
 };
