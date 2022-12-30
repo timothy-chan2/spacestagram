5f00868e56bfc96a2164cce176bfdf4cf29bc037
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-12-29 19:27:56 -0500

    Add component used for selecting an alternate loading icon

---
+++ src/components/SelectLoadingIcon.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+const SelectLoadingIcon = (props) => {
+  const { setLoadingIcon } = props;
+  const [visible, setVisible] = useState(false);
+
+  const toggleLoadingIconDropdown = () => setVisible(prev => !prev);
+
+  return (
+    <article>
+      <button
+        aria-label='Switch to a different loading icon'
+        onClick={toggleLoadingIconDropdown}
+        className='loading-dropdown-btn'
+        type='button'
+        aria-controls='loading-choices'
+      >
+        Loading Icon
+      </button>
+      {visible ? (
+        <section id='loading-choices'>
+          <button
+            aria-label='Switch to the dots loading icon'
+            onClick={setLoadingIcon('dots')}
+            className='loading-option-btn'
+            type='button'
+          >
+            Dots
+          </button>
+          <button
+            aria-label='Switch to the wheel loading icon'
+            onClick={setLoadingIcon('wheel')}
+            className='loading-option-btn'
+            type='button'
+          >
+            Wheel
+          </button>
+        </section>
+      ) : null}
+    </article>
+  );
+};
+
+export default SelectLoadingIcon;
