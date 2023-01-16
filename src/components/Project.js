4f44af62ac4d3e40fdaeee185850a5955e4817d4
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-01-15 21:28:43 -0500

    Change the image element to only render if linkUrl exists in preparation to include a video instead of an image

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,14 +3,16 @@
 const Project = (props) => {
   return (
     <article className='post'>
-      <a href={props.linkUrl} target='_blank' rel='noreferrer'>
-        <img
-          src={props.imgUrl}
-          aria-label={props.title}
-          className='post-img'
-          loading='lazy'
-        />
-      </a>
+      {props.linkUrl &&
+        <a href={props.linkUrl} target='_blank' rel='noreferrer'>
+          <img
+            src={props.imgUrl}
+            aria-label={props.title}
+            className='post-img'
+            loading='lazy'
+          />
+        </a>
+      }
       <h3 className='post-title'>{props.title}</h3>
       <p className='post-desc project-desc'>{props.description}</p>
     </article>
